40db55c5e07d3a919bab386810127209f5d56f17
Austin <amayes1991@gmail.com>
2022-04-18 11:32:59 -0500

    third

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-///method overide
+
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
+
     let method = req.body._method
     delete req.body._method
     return method
